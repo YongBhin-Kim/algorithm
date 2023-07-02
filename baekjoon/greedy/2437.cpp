0ad8e25c927cb37a0dccac67742d2857185d1019
YongBhinKim <coji67@gmail.com>
2023-07-02 23:37:42 +0900

    greedy algorithm

---
+++ baekjoon/greedy/2437.cpp
@@ -0,0 +1,31 @@
+// 백준 2437. 저울(골1) 23.06.28
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int weight[1000];
+
+int main() {
+
+    cin>>N;
+    for (int i=0; i<N; i++) {
+        cin>>weight[i];
+    }
+    sort(weight, weight+N);
+
+    int res=1;
+    for (int i=0; i<N; i++) {
+        if ( weight[i]>res ) {
+            break;
+        }
+        res+=weight[i];
+    }
+    cout << res << endl;
+
+    return 0;
+}
+
+// 1 3 --> 2
+// 1 2 4 --> 3
