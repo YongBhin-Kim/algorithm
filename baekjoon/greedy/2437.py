0842effbb2aa0e683a29434be72f4aec04a498d5
YongBhinKim <coji67@gmail.com>
2023-07-03 13:22:14 +0900

    greedy algorithm

---
+++ baekjoon/greedy/2437.py
@@ -0,0 +1,12 @@
+# 백준 2437. 저울(골1) 23.07.03
+
+N = int(input())
+weight = list(map(int, input().split()))
+weight.sort()
+
+res = 1
+for i in weight:
+    if i > res:
+        break
+    res+=i
+print(res)
